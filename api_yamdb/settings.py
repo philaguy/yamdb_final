94cf51151cec9913011d69085b928ecf9db55b5a
root <root@philaguy.ru-central1.internal>
2022-04-22 08:59:34 +0000

    поправил имя по-умолчанию для db

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "api_yamdb.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME"),
+        "NAME": os.getenv("DB_NAME", "postgres"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
